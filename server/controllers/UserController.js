ea63cb13aa26fc7dd13e3ef555f97ba51c7f2370
Menghao Yu <menghaoyu2002@gmail.com>
2021-06-20 18:55:52 -0400

    fixed delete user bug

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -74,7 +74,7 @@ exports.deleteUser = async function (req, res, next) {
     user.commentHistory.forEach(async (commentId) => {
       let comment = await Comment.findById(commentId);
       comment.body = 'User Deleted';
-      comment.author = 'Deleted';
+      comment.authorName = 'Deleted User';
       comment.save();
     });
 
