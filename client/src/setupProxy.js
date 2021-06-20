36e37f8e4568004bc91bf68619309ff82c42072c
Menghao Yu <menghaoyu2002@gmail.com>
2021-06-20 14:44:56 -0400

    deployment changes

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,9 +1,8 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/api',
-    createProxyMiddleware({
+    proxy('/api', {
       target: 'http://localhost:3000',
       changeOrigin: true,
     })
