a2b198a95bbfca86cfe1a099638f132e7f9a83f1
Menghao Yu <menghaoyu2002@gmail.com>
2021-06-20 16:16:59 -0400

    deployment changes

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://interpoll-backend.herokuapp.com';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
